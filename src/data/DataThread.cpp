d08406a77c1c83f0c4fbaa05f4fa3325ba3a407d
Bryan Bendall <b_bendall@hotmail.com>
2024-11-12 13:37:54 -0500

    Process can commands faster

--- src/data/DataThread.cpp
+++ src/data/DataThread.cpp
@@ -23,11 +23,21 @@ void DataThread::run()
     Brytec::EBrytecApp::initalize();
     Server server(m_io_context);
 
+    uint8_t count = 0;
+
     while (m_run) {
         m_io_context.poll();
         server.checkConnection();
-        Brytec::EBrytecApp::update(10);
-        std::this_thread::sleep_for(10ms);
+
+        if (count >= 10) {
+            Brytec::EBrytecApp::update(10);
+            count = 0;
+        } else {
+            Brytec::EBrytecApp::processCanCommands();
+        }
+
+        std::this_thread::sleep_for(1ms);
+        count++;
     }
 }
 
