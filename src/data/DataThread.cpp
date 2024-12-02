e775495f8b69402f1675b55a50871a42d81934f3
Bryan Bendall <b_bendall@hotmail.com>
2024-12-02 17:09:23 -0500

    Updated data thread with delta time clock

--- src/data/DataThread.cpp
+++ src/data/DataThread.cpp
@@ -3,6 +3,7 @@
 #include "BrytecConfigEmbedded/EBrytecApp.h"
 #include "data/communication/can/CanManager.h"
 #include "data/communication/wifi/Server.h"
+#include <chrono>
 
 using namespace std::chrono_literals;
 
@@ -24,22 +25,24 @@ void DataThread::run()
     Brytec::EBrytecApp::initalize();
     Server server(m_io_context);
 
-    uint8_t count = 0;
+    std::chrono::steady_clock::time_point lastUpdate;
 
     while (m_run) {
         m_io_context.poll();
         CanManager::poll();
         server.checkConnection();
 
-        if (count >= 10) {
-            Brytec::EBrytecApp::update(10);
-            count = 0;
+        auto now = std::chrono::steady_clock::now();
+        float deltaTime = std::chrono::duration_cast<std::chrono::milliseconds>(now - lastUpdate).count();
+
+        if (deltaTime >= 10.0f) {
+            Brytec::EBrytecApp::update((uint32_t)deltaTime);
+            lastUpdate = now;
         } else {
             Brytec::EBrytecApp::processCanCommands();
         }
 
         std::this_thread::sleep_for(1ms);
-        count++;
     }
 }
 
