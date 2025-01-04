1784b96ea3dc9807a66795e164eb168c3c27fe17
Bryan Bendall <b_bendall@hotmail.com>
2025-01-04 00:09:15 -0500

    Work on setup script

--- resources/scripts/setup.sh
+++ resources/scripts/setup.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-pkgs=(git build-essential cmake libsdl2-dev libopencv-dev)
+pkgs=(git build-essential cmake libopencv-dev libdrm-dev libudev-dev libasound2-dev libdbus-1-dev libglfw3-dev libgles2-mesa-dev)
 sudo apt-get -y --ignore-missing install "${pkgs[@]}" 
+
+git clone https://github.com/libsdl-org/SDL.git -b release-2.30.x
+mkdir build
+cd build
+../configure --enable-video-kmsdrm
+make
+sudo make install
+cd ../../
+
 
 git clone --recursive https://github.com/bryanbendall/RaspberryPiScreen.git
