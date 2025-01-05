e952a58bae535bba28b5cf4e02af5d845459f4de
Bryan Bendall <b_bendall@hotmail.com>
2025-01-04 22:30:09 -0500

    Updates to setup script

--- resources/scripts/setup.sh
+++ resources/scripts/setup.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-pkgs=(git build-essential cmake libopencv-dev libdrm-dev libudev-dev libasound2-dev libdbus-1-dev libglfw3-dev libgles2-mesa-dev)
+sudo apt-get update
+pkgs=(git build-essential cmake libopencv-dev libdrm-dev libudev-dev libasound2-dev libdbus-1-dev libglfw3-dev libgles2-mesa-dev libgbm-dev libraspberrypi-bin libraspberrypi-dev)
 sudo apt-get -y --ignore-missing install "${pkgs[@]}" 
 
 git clone https://github.com/libsdl-org/SDL.git -b release-2.30.x
+cd SDL
 mkdir build
 cd build
-../configure --enable-video-kmsdrm
+../configure --enable-video-kmsdrm --disable-video-x11
 make
 sudo make install
 cd ../../
 
-
 git clone --recursive https://github.com/bryanbendall/RaspberryPiScreen.git
+
+# Use this command to be able to use port 80 after binary is built
+# sudo setcap 'cap_net_bind_service=+ep' /home/pi/RaspberryPiScreen/build/RaspberryPiScreen
